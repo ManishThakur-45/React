91975cc933add64d18e5286c7956623f4288e4a6
Manish Kumar Thakur <manishsatya2004@gmail.com>
2026-08-30 22:37:18 +0530

    first commit

---
+++ class4/src/App.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import Navbar from "./components/Navbar";
+import Men from "./components/Men";
+import Women from "./components/Women";
+
+const App = () => {
+
+  function btnClicked(a) {
+    console.log('Button is Clicked!!!', a);
+  }
+
+  return (
+    <div>
+
+      <Navbar
+        color="black"
+        title="My Website"
+        links={["Home", "About", "Contact"]}
+      />
+
+      <Men />
+
+      <Women />
+
+      <button
+        onClick={function () {
+          btnClicked(10);
+        }}
+        className='active:scale-95 bg-emerald-600 text-white px-6 py-3 rounded font-bold m-2'
+      >
+        Click to Download
+      </button>
+
+    </div>
+  );
+}
+
+export default App;
